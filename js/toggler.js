969ea98c3b24e39aea1311eac13c3fdccd508e35
MSoler <msolerpl10@gmail.com>
2018-03-21 22:55:16 +0100

    Change active tab on navbar

--- js/toggler.js
+++ js/toggler.js
@@ -2,7 +2,10 @@
 $(document).ready(function() {
     $("#page").load("contents/home.html");
     $("ul.navbar-nav li").each(function() {
-        $(this).click(function(e) { $("#page").load("contents/"+$(this).attr("loadpage")+".html");
+        $(this).click(function(e) {
+            $("#page").load("contents/"+$(this).attr("loadpage")+".html");
+            $("ul.navbar-nav li").removeClass("active");
+            $(this).addClass("active");
         });
     });
 });

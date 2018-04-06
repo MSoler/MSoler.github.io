af40e930d492f7a7dac8ac4a4da128771c602763
MSoler <msolerpl10@gmail.com>
2018-04-06 21:14:13 +0200

    Add content project page

--- js/carousel.js
+++ js/carousel.js
@@ -1,14 +1,18 @@
-$('.carousel video').on("stop ended", function(e) {
+$('.carousel').carousel({
+  interval: 4000
+})
+$('.carouselVid video').on("stop ended", function(e) {
     var carItem = $(e.target).parent();
     if(carItem.hasClass("active")){
         carItem.parent().carousel('next');
         carItem.parent().carousel('pause'); //Needs to be added due to https://github.com/twbs/bootstrap/issues/5747
     }
 });
-$('.carousel').on("slid.bs.carousel",function(e){
+$('.carouselVid').on("slid.bs.carousel",function(e){
+    $(this).carousel("pause");
     $(e.target).find("video").each(function(){
         this.pause();
         this.currentTime=0;
     });
-    var video = $(e.target).find(".active").children().first().get(0).play();
+    $(e.target).find(".active").children().first().get(0).play();
 });
